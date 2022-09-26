843771847ae5e34f836e37b6a138e080ece448fe
FrancoMostafa <francom2342@gmail.com>
2022-09-26 20:27:50 -0300

    revancha

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -14,6 +14,7 @@ for (var c = 0; c < 7; c++) {
 }
 
 const playerId = (Math.random() + 1).toString(36).substring(7);
+let rematchConfirm = false;
 
 export default function Game() {
   // eslint-disable-next-line no-unused-vars
@@ -80,10 +81,9 @@ export default function Game() {
     } else if (message.detail === "DISCONNECT") {
       SwalDisconnectOpponent();
     } else if (message.detail === "CHAT") {
-      setChat((chat) => [
-        ...chat,
-        { user: message.data.user, text: message.data.text },
-      ]);
+      const user = message.data.user;
+      const text = message.data.text;
+      setChat((chat) => [...chat, { user: user, text: text }]);
     } else if (message.detail === "MOVE") {
       const pieceData = message.data[0];
       const playerNumberData = message.data[1];
@@ -95,10 +95,39 @@ export default function Game() {
       const roundResultColorWin = roundResult[1];
       if (roundResultEvaluate) {
         playersData = addWinToPlayer(roundResultColorWin, playersData);
-        SwalRoundWinner(roundResultColorWin, playersData);
+        SwalRoundWinner(roundResultColorWin, playersData, SwalRematch);
         resetBoard();
       }
       changeTurn(turnData);
+    } else {
+      // REMATCH
+      const rematchAcceptedIncomingValue = message.data[0];
+      const rematchAcceptedIncomingId = message.data[1];
+      const rematchAcceptedIncomingResult = message.data[2];
+      if (!rematchAcceptedIncomingValue) {
+        // REMATCH NOT ACCEPTED
+        window.location = window.location.origin;
+      } else if (rematchAcceptedIncomingResult) {
+        // REMATCH START
+        setPlayers({
+          player1Name: players.player1Name,
+          player1Id: players.player1Id,
+          player1Wins: 0,
+          player2Name: players.player2Name,
+          player2Id: players.player2Id,
+          player2Wins: 0,
+        });
+        resetBoard();
+        SwalStart();
+      } else if (rematchConfirm && rematchAcceptedIncomingId !== playerId) {
+        // REMATCH ACCEPTED
+        ws_game.send(
+          CreateMessageGame(gameId, [true, playerId, true], "REMATCH")
+        );
+      } else {
+        // REMATCH CONFIRM
+        rematchConfirm = true;
+      }
     }
   };
 
@@ -382,6 +411,36 @@ export default function Game() {
     return newPlayersData;
   };
 
+  const SwalRematch = () => {
+    return Swal.fire({
+      title: `Revancha?`,
+      heightAuto: false,
+      allowOutsideClick: false,
+      showCancelButton: true,
+      showConfirmButton: true,
+      confirmButtonText: "Si",
+      cancelButtonText: "No",
+      confirmButtonColor: "green",
+      cancelButtonColor: "red",
+      backdrop: `
+    rgba(0, 0, 0, 0.8)
+    left top
+    no-repeat
+  `,
+    }).then((result) => {
+      ws_game.send(
+        CreateMessageGame(
+          gameId,
+          [result.isConfirmed, playerId, false],
+          "REMATCH"
+        )
+      );
+      if (result.isConfirmed) {
+        SwalRematchWaiting();
+      }
+    });
+  };
+
   const ConnectFourGame = () => {
     return (
       <div className="board">
@@ -512,7 +571,7 @@ const SwalDisconnectOpponent = () => {
   });
 };
 
-const SwalRoundWinner = (winnerColor, pData) => {
+const SwalRoundWinner = (winnerColor, pData, swalRematch) => {
   let winnerRoundName;
   if (winnerColor === "rojo") {
     winnerRoundName = pData.player1Name;
@@ -533,14 +592,14 @@ const SwalRoundWinner = (winnerColor, pData) => {
   `,
   }).then((result) => {
     if (result.dismiss === Swal.DismissReason.timer) {
-      if (pData.player1Wins === 3 || pData.player2Wins === 3) {
-        SwalPlayerWinner(winnerRoundName);
+      if (pData.player1Wins === 1 || pData.player2Wins === 1) {
+        SwalPlayerWinner(winnerRoundName, swalRematch);
       }
     }
   });
 };
 
-const SwalPlayerWinner = (pName) => {
+const SwalPlayerWinner = (pName, swalRematch) => {
   return Swal.fire({
     title: `${pName} gana la partida!`,
     heightAuto: false,
@@ -555,7 +614,25 @@ const SwalPlayerWinner = (pName) => {
   `,
   }).then((result) => {
     if (result.dismiss === Swal.DismissReason.timer) {
-      window.location = window.location.origin;
+      swalRematch();
     }
+  });
+};
+
+const SwalRematchWaiting = () => {
+  return Swal.fire({
+    title: `Esperando confirmacion de oponente...`,
+    heightAuto: false,
+    allowOutsideClick: false,
+    showCancelButton: false,
+    showConfirmButton: false,
+    didOpen: () => {
+      Swal.showLoading();
+    },
+    backdrop: `
+    rgba(0, 0, 0, 0.8)
+    left top
+    no-repeat
+  `,
   });
 };
